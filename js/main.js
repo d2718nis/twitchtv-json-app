83e1c3e95f9ecc6b5cd3f20eda5d6f281ebd081a
Denis Zharikov <d2718nis@gmail.com>
2017-05-04 02:05:43 +0300

    Fix no channel logo path (null)

--- js/main.js
+++ js/main.js
@@ -82,7 +82,7 @@ function displayChannelsLive() {
 					</div>
 					<div class="channel-info">
 						<div class="channel-logo">
-							<img class="img-responsive logo-img" src="${item.json.stream.channel.logo}" alt="${item.json.stream.channel.display_name}">
+							<img class="img-responsive logo-img" src="${item.json.stream.channel.logo===null?'https://placehold.it/100?text=User':item.json.stream.channel.logo}" alt="${item.json.stream.channel.display_name}">
 						</div>
 						<div class="channel-text">
 							<span class="channel-name">${item.json.stream.channel.display_name}</span>
@@ -110,7 +110,7 @@ function displayChannelsOffline() {
 					<div class="col-xs-12 col-sm-6 col-md-4 channel-item">
 						<div class="channel-info">
 							<div class="channel-logo">
-								<img class="img-responsive logo-img" src="${item.json.logo}" alt="${item.json.display_name}">
+								<img class="img-responsive logo-img" src="${item.json.logo===null?'https://placehold.it/100?text=User':item.json.logo}" alt="${item.json.display_name}">
 							</div>
 							<div class="channel-text">
 								<span class="channel-name">${item.json.display_name}</span>
